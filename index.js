adbd39ae345d83a1dea2c91d8de94e4523d044b6
Ene-Daniele <ene.daniele@iisgalvanimi.edu.it>
2021-11-04 13:42:39 +0000

    thing i tried

--- index.js
+++ index.js
@@ -7,3 +7,31 @@ function howFat(urmom) {
 }
 
 howFat(yourmom)
+
+thing = new Truee(17, "your mom");
+
+console.log(thing.getAge);
+console.log(thing.name);
+
+class Truee {
+     let age;
+     let name;
+    Truee(age, name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    getAge() {
+        return age;
+    }
+    getName() {
+        return name;
+    }
+
+    setAge(newAge) {
+        this.age = newAge;
+    }
+    setName(newName) {
+        this.name = newName;
+    }
+}
